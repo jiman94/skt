fa601500471add364683585f0a2280e03e41dae0
anisia20 <anisia20@nate.com>
2020-10-16 18:37:39 +0900

    add csv to redis

--- cqrs-redis-product-api/core/src/main/java/oss/core/infra/InfraConstants.java
+++ cqrs-redis-product-api/core/src/main/java/oss/core/infra/InfraConstants.java
@@ -12,4 +12,7 @@ public class InfraConstants {
 	public static final String EVENTROW_Z_ORDER = "EVENTROW_Z_MEMBER";
 	public static final String EVENTROW_Z_PRODUCT = "EVENTROW_Z_MEMBER";
 	public static final String USER_H_KEY = "USER_H_KEY";
+	
+	//product
+	public static final String CREATE_SCV_Q_PRODUCT = "CREATE_SCV_Q_PRODUCT";
 }
